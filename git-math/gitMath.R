38067ec8f2ed75d0e884a483f53de8dd0b17c36d
Noah Gift <noah.gift@gmail.com>
2013-02-18 17:50:43 -0800

    adding report capability to code metrics in R

--- git-math/gitMath.R
+++ git-math/gitMath.R
@@ -3,13 +3,17 @@
 
 #Usage example:
 #source('/Users/noah/src/boundary/git-math/gitMath.R')
-#gs <- churn()
+#gs <- mkchurnline()	#lines, churn, churn/lines
 #OR
-#churnplot()
+#mkreport()				#generates report of top values, all and a plot of top values
+#gitMathAll.txt and gitMathtop.txt is created in current working directory
+#also creates pdf called topchurn.pdf
 
 #TO DO
-#1.  Consider what to do with files that have never changed (1 value)
+#1.  Consider what to do with files that have never changed (1 value), currently they are discarded
 #2.  Break out a new matrix for each file extension
+#3.  Figure out what to do for file counts on historical churn metrics
+#4.  Fix divide by zero infinity in relcodechurn
 
 #Get linecount from file
 linecount <- function(filename){
@@ -72,16 +76,44 @@ churnline <- function (cmatrix, lmatrix){
 	return(churnlinematrix)
 }
 
+#Generates relative churn/loc metric and adds to matrix
+relchurn <- function (clc) {
+	clc <- cbind(clc,clc$churn/clc$linecount)
+	return(clc)
+}
+
 #Generates churn/line matrix and returns merged matrix
+#Omits NA values..i.e. files no longer in repo
 mkchurnline <- function () {
 	cmatrix <- churn()
 	lmatrix <- mklinecount(cmatrix)
 	clcmatrix <- churnline(cmatrix, lmatrix)
 	clcmatrix <- na.omit(clcmatrix)
 	colnames(clcmatrix) <- c("files", "churn", "linecount")
+	clcmatrix.relchurn <- clcmatrix$churn/clcmatrix$linecount
+	clcmatrix <- cbind(clcmatrix, clcmatrix.relchurn)
+	colnames(clcmatrix) <- c("files", "churn", "linecount", "relchurn")
 	return(clcmatrix)
 }
 
+#Generate top values for report, default to top 25
+mkreport <- function (topval=25){
+	clc <- mkchurnline()
+	#order matrix by highest churn file
+	clco <- clc[order(clc$churn, decreasing=TRUE),]
+	clcotop <- clco[1:topval,]
+	plot(clcotop$churn, clcotop$relchurn, main="Churn vs relchurn", xlab="Churn", ylab="Churn/LOC", pch=18,col="blue")
+	text(clcotop$churn, clcotop$relchurn, clcotop$files, cex=0.8, pos=4, col="red")
+	#now save pdf
+	pdf(file="topchurn.pdf")
+	plot(clcotop$churn, clcotop$relchurn, main="Churn vs relchurn", xlab="Churn", ylab="Churn/LOC", pch=18,col="blue")
+	text(clcotop$churn, clcotop$relchurn, clcotop$files, cex=0.8, pos=4, col="red")
+	dev.off()
+	#write report
+	write.table(clc, file="gitMathAll.txt")
+	write.table(clcotop, file="gitMathtop.txt")
+
+}
 churnplot <- function() {
 	plot(churn(), ylab="file churn", xlab="file count")
 }
