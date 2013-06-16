673a70ed062133333bccb896db797bc5181a5e1a
noah <noah@ubuntu.(none)>
2013-06-16 12:53:49 -0700

    Fixing indention

--- Sim.R
+++ Sim.R
@@ -16,10 +16,10 @@ sim <-
     if (missing(prodcoeff)) prodcoeff <- 0.6
     x <- matrix(runif(2*n),ncol=2)
     geny <- function(xrow) {
-        x1 <- xrow[1]
-        x2 <- xrow[2]
-        tmp <- runif(1) < (x1 + x2 + prodcoeff*x1*x2)/3
-        as.integer(tmp)
+    x1 <- xrow[1]
+    x2 <- xrow[2]
+    tmp <- runif(1) < (x1 + x2 + prodcoeff*x1*x2)/3
+    as.integer(tmp)
 }
     y <- apply(x,1,geny)
     cbind(x,y)
