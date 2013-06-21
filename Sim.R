d4edca77fee76d062a339231d46ee624330968cb
Noah Gift <noah.gift@gmail.com>
2013-06-20 21:56:27 -0700

    Adding Sim.R global variable fix

--- Sim.R
+++ Sim.R
@@ -6,7 +6,7 @@ bstrapsnow <-
    function(csize=4) 
 {
       shmcls <- function(k) makeCluster(type = "SOCK", rep("localhost", k))
-      c2 <- shmcls(csize)
+      shmcls(csize)
 }
 
 sim <-
@@ -43,38 +43,38 @@ runsim <-
 ##K Values######
 #Example Run For N=10000
 #For deriving K graph
-# bet <- 0.6
-# dta <- sim(10000, bet)
-# p1 <- runsim(dta, bandhw=0.4)
-# p1a <- p1 + annotate("text", x=0.01, y=.637, label="k=4")
-# p2 <- runsim(dta, k=40, oldplot=p1a)
-# p2a <- p2 + annotate("text", x=0.01, y=0.850, label="k=40")
-# p3 <- runsim(dta, k=400, oldplot=p2a)
-# p3a <- p3 + annotate("text", x=0.01, y=0.77, label="k=400")
-# pf <- p3a + annotate("text", x=.5, y=0.2, label="N=10000, prodcoeff=0.6")
-# #True Curve
-# b <- mean(dta[,3])
-# pff <- pf + stat_function(fun=function(x) (3*b-x)/(1+bet*x))
-# pffa <- pff + annotate("text", x=.035, y=1.2, label="True Curve")
+bet <- 0.6
+dta <- sim(10000, bet)
+p1 <- runsim(dta, bandhw=0.4)
+p1a <- p1 + annotate("text", x=0.01, y=.637, label="k=4")
+p2 <- runsim(dta, k=40, oldplot=p1a)
+p2a <- p2 + annotate("text", x=0.01, y=0.850, label="k=40")
+p3 <- runsim(dta, k=400, oldplot=p2a)
+p3a <- p3 + annotate("text", x=0.01, y=0.77, label="k=400")
+pf <- p3a + annotate("text", x=.5, y=0.2, label="N=10000, prodcoeff=0.6")
+#True Curve
+b <- mean(dta[,3])
+pff <- pf + stat_function(fun=function(x) (3*b-x)/(1+bet*x))
+pffa <- pff + annotate("text", x=.035, y=1.2, label="True Curve")
 
 
 ##K Values######
 #Example Run For N=100000
 #For deriving K graph
 #Still Crashes on 64 bit R on OS X
-# bet <- 0.6
-# dta <- sim(100000, bet)
-# p1 <- runsim(dta, bandhw=0.4)
-# p1a <- p1 + annotate("text", x=0.01, y=.637, label="k=4")
-# p2 <- runsim(dta, k=40, oldplot=p1a)
-# p2a <- p2 + annotate("text", x=0.01, y=0.850, label="k=40")
-# p3 <- runsim(dta, k=400, oldplot=p2a)
-# p3a <- p3 + annotate("text", x=0.01, y=0.77, label="k=400")
-# pf <- p3a + annotate("text", x=.5, y=0.2, label="N=100000, prodcoeff=0.6")
-# #True Curve
-# b <- mean(dta[,3])
-# pff <- pf + stat_function(fun=function(x) (3*b-x)/(1+bet*x))
-# pffa <- pff + annotate("text", x=.035, y=1.2, label="True Curve")
+bet <- 0.6
+dta <- sim(100000, bet)
+p1 <- runsim(dta, bandhw=0.4)
+p1a <- p1 + annotate("text", x=0.01, y=.637, label="k=4")
+p2 <- runsim(dta, k=40, oldplot=p1a)
+p2a <- p2 + annotate("text", x=0.01, y=0.850, label="k=40")
+p3 <- runsim(dta, k=400, oldplot=p2a)
+p3a <- p3 + annotate("text", x=0.01, y=0.77, label="k=400")
+pf <- p3a + annotate("text", x=.5, y=0.2, label="N=100000, prodcoeff=0.6")
+#True Curve
+b <- mean(dta[,3])
+pff <- pf + stat_function(fun=function(x) (3*b-x)/(1+bet*x))
+pffa <- pff + annotate("text", x=.035, y=1.2, label="True Curve")
 
 
 ##BANDHW#####
